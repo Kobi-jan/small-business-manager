67529e36a0c009808b1c836adb620a3ef9316637
Janice <122884169+Kobi-jan@users.noreply.github.com>
2025-01-04 18:30:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((err, req, res, next) => {
 
 // Landing page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); 
+    res.sendFile(path.join(__dirname, 'views' 'index.html')); 
 });
 
 // Routes
